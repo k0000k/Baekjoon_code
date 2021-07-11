b53d786bc32c2811a20ffc8063240fd3e906a422
k0000k <84006880+k0000k@users.noreply.github.com>
2021-07-11 13:18:46 +0900

    A+B

---
+++ 1000번.c
@@ -0,0 +1,13 @@
+/*
+1000번
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+*/
+#include <stdio.h>
+
+int main(void) {
+	int A, B;
+	scanf("%d", &A);
+	scanf("%d", &B);
+	printf("%d", A + B);
+	return 0;
+}
